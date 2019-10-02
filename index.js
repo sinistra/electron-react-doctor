0cca525952f62b76a306d0ce3d7b10aeb98ce161
Paul Taylor <ptaylor@ricoh.com.au>
2019-10-03 08:24:48 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const electron = require('electron');
+const fs = require('fs');
+const uuid = require('uuid');
+
+const { app, BrowserWindow, ipcMain, Menu } = electron;
+
+let mainWindow;
+let allAppointments = [];
+
+fs.readFile('db.json', (err, jsonAppointments) => {
+    if (!err) {
+        const oldAppointments = JSON.parse(jsonAppointments);
+        allAppointments = oldAppointments;
+    }
+});
+
+const createWindow = () => {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        title: 'Doctor Appointments',
+        resizable: false
+    });
+
+    const startUrl = process.env.ELECTRON_START_URL || `file://${__dirname}/build/index.html`;
+    mainWindow.loadURL(startUrl);
+
+    mainWindow.on('closed', () => {
+        const jsonAppointments = JSON.stringify(allAppointments);
+        fs.writeFileSync('db.json', jsonAppointments);
+        app.quit();
+        mainWindow = null;
+    });
+
+    if (process.env.ELECTRON_START_URL) {
+        const mainMenu = Menu.buildFromTemplate(menuTemplate);
+        Menu.setApplicationMenu(mainMenu);
+    } else {
+        Menu.setApplicationMenu(null);
+    }
+};
+
+const menuTemplate = [
+    {
+        label: 'View',
+        submenu: [{role: 'reload'}, {role: 'toggledevtools'}]
+    }
+];
+
+ipcMain.on("appointment:create", (event, appointment) => {
+    appointment["id"] = uuid();
+    appointment["done"] = 0;
+    allAppointments.push(appointment);
+  
+    // sendTodayAppointments();
+    // createWindow.close();
+  });
+  
+  ipcMain.on("appointment:request:list", event => {
+    mainWindow.webContents.send("appointment:response:list", allAppointments);
+  });
+  
+  ipcMain.on("appointment:request:today", event => {
+    sendTodayAppointments();
+  });
+  
+  ipcMain.on("appointment:done", (event, id) => {
+    allAppointments.forEach(appointment => {
+      if (appointment.id === id) appointment.done = 1;
+    });
+  
+    sendTodayAppointments();
+  });
+  
+  const sendTodayAppointments = () => {
+    const today = new Date().toISOString().slice(0, 10);
+    const filtered = allAppointments.filter(
+      appointment => appointment.date === today
+    );
+    mainWindow.webContents.send("appointment:response:today", filtered);
+  };
+
+app.on('ready', createWindow);
