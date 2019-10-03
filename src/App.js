f2e2a1212b7f5c57b2245ea3d261e45a985930dc
Paul Taylor <ptaylor@ricoh.com.au>
2019-10-04 09:34:29 +1000

    complete react version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SideBar from "./components/SideBar";
+import CreateAppointment from "./components/appointment/Create";
+import AppointmentList from "./components/appointment/List";
+import TodayAppointments from "./components/appointment/Today";
+
+class App extends Component {
+  state = {
+    view: "today"
+  };
+
+  renderView() {
+    if (this.state.view === "create") return <CreateAppointment />;
+    else if (this.state.view === "list") return <AppointmentList />;
+    else return <TodayAppointments />;
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col md="3">
+            <SideBar
+              onViewChange={view => this.setState({ view })}
+              selected={this.state.view}
+            />
+          </Col>
+          <Col md="9">{this.renderView()}</Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
